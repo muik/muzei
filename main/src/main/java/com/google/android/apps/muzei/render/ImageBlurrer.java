249fad868ac2e75e7430b9330bf85b5606b06341
Mu-ik Jeon <muikor@gmail.com>
2014-04-22 10:30:57 +0900

    Change to minSdkVersion 16

--- main/src/main/java/com/google/android/apps/muzei/render/ImageBlurrer.java
+++ main/src/main/java/com/google/android/apps/muzei/render/ImageBlurrer.java
@@ -18,10 +18,10 @@ package com.google.android.apps.muzei.render;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.renderscript.Allocation;
-import android.renderscript.Element;
-import android.renderscript.RenderScript;
-import android.renderscript.ScriptIntrinsicBlur;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.Element;
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsicBlur;
 
 public class ImageBlurrer {
     public static final int MAX_SUPPORTED_BLUR_PIXELS = 25;
